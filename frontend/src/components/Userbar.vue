73ce51b8ca6c042eceeedcd9a644e5f8f0cd8392
Alexandra <59907792+ajvtuliao@users.noreply.github.com>
2022-04-02 05:16:54 +0800

    created a navbar for home page

---
+++ frontend/src/components/Userbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app-bar
+      color="#1e2328"
+      dark
+      height="80px"
+    >
+    <!-- Icon -->
+      <v-col
+        cols="1"
+        class="mt-4"
+      >
+        <!-- <v-img 
+          :src="require('../assets/liblogo.svg')"
+          contain
+          height="70px"
+          width="65px"
+        /> -->
+      </v-col>
+      <!-- Title -->
+      <v-col>
+
+      </v-col>
+      <!-- Nav Buttons -->
+    </v-app-bar>
+</template>
+    
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
