6ae9e448626e95f787c4a177c698efeb7451bc72
John Henry Galino <jhgalino@disroot.org>
2022-04-03 00:31:42 +0800

    Fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,62 @@
 <template>
   <v-app style="background: #faf4e6">
     <v-main>
-      <router-view/>
+<v-app-bar
+        color="#1e2328"
+        dark
+        height="115 em"
+        src="./assets/appban.png"
+        fade-img-on-scroll
+        scroll-target="#scrolling-techniques-3"
+        style="position: -webkit-sticky; position: sticky; top: 0em; z-index: 2"
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(45, 51, 56,.7), rgba(30, 35, 40)"
+          ></v-img>
+        </template>
+        <!-- Icon -->
+        <v-col cols="1" class="mb-1">
+          <v-img
+            :src="require('./assets/liblogo.svg')"
+            contain
+            height="120px"
+            width="110px"
+          />
+        </v-col>
+        <!-- Title -->
+        <v-col cols="7" class="mb-1">
+          <div class="mb-n6">
+            <div
+              class="font-weight-bold display-2 text-decoration-underline mr-3"
+            >
+              UP SLIS Library
+              <hr color="#fac750" size="3px" width="336 em" />
+            </div>
+          </div>
+          <br />
+          <span class="font-weight-light display-2"> Digital Hub</span>
+        </v-col>
+        <!-- Nav Buttons -->
+        <v-col cols="4" class="mt-3 ml-n2">
+          <v-row align="center">
+            <v-btn text color="#faf4e6"> Home </v-btn>
+            <v-btn text color="#faf4e6"> Collections </v-btn>
+            <v-btn text color="#faf4e6"> About </v-btn>
+            <v-btn text color="#faf4e6">
+              <v-icon class="mr-1"> mdi-login </v-icon>
+              Login
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-app-bar>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
 
@@ -17,5 +66,4 @@ export default {
 };
 </script>
 <style scoped>
-
 </style>
---
+++ frontend/src/views/Collections.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Hello</h1>
+        <h1>Hi</h1>
+        <h3>COCOCO</h3>
+    </div>
+</template>
+
