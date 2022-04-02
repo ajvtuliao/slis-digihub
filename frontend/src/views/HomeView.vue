a915c61a548eb353429ef0d74ebf53bd7dad02f3
Alexandra <59907792+ajvtuliao@users.noreply.github.com>
2022-04-03 04:11:02 +0800

    added carousel in home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,39 @@
 <template>
-  
+  <v-carousel 
+    cycle
+    interval="2000"
+    show-arrows
+    hide-delimiter-background
+  >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
   export default {
-    
+    data () {
+      return {
+        items: [
+          {
+            src: '../assets/appban.png',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ]
+      }
+    }
   }
 </script>
