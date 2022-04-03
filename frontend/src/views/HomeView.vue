438ee54db47de733ef4800a261dd871b6b6fafc4
Alexandra <59907792+ajvtuliao@users.noreply.github.com>
2022-04-03 17:59:57 +0800

    updated carousel

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <v-carousel 
+  <div>
+    <v-carousel 
     cycle
     interval="2000"
     show-arrows
     hide-delimiter-background
-  >
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-      reverse-transition="fade-transition"
-      transition="fade-transition"
-    ></v-carousel-item>
-  </v-carousel>
+    >
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
 <script>
