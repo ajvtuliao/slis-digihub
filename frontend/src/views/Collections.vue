9881d153265e78e7e2930199787d1d4a008a3b24
Alexandra <59907792+ajvtuliao@users.noreply.github.com>
2022-04-04 03:39:53 +0800

    updated collection page

--- frontend/src/views/Collections.vue
+++ frontend/src/views/Collections.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
-        <h1>Hello</h1>
-        <h1>Hi</h1>
-        <h3>COCOCO</h3>
+       
     </div>
 </template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
 
