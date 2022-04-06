449f8d7daf0a4583c40956205e1ab0cca10da808
Alexandra <59907792+ajvtuliao@users.noreply.github.com>
2022-04-06 17:13:21 +0800

    added buttons and table

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,3 +1,82 @@
 <template>
-    <div>Admin</div>
+    <div>
+        <v-row class="justify-content-center align-center mx-5 mt-3">
+            <v-col cols="9">
+                <v-text-field 
+                    class="search"
+                    placeholder="Search Collections"
+                    outlined
+                    dense
+                    prepend-inner-icon="mdi-magnify"
+                    clearable
+                    v-model="search"
+                >
+                </v-text-field>
+            </v-col>
+            <v-col cols="1">
+                <v-btn class="mt-n7 px-2" model="addBook" color="green" dark>Add Books</v-btn>
+            </v-col>
+            <v-col cols="1">
+                <v-btn class="mt-n7 px-2" model="addInCarousel" color="green" dark>Add in Carousel</v-btn>
+            </v-col>
+        </v-row>
+        <v-row class="mx-n12" align="center" justify="center">
+            <v-simple-table class="table" fixed-header dark>
+                <template v-slot:default>
+                    <thead>
+                        <tr>
+                            <th class="text-center">Title</th>
+                            <th class="text-center">Author</th>
+                            <th class="text-center">Publisher</th>
+                            <th class="text-center">Year</th>
+                            <th class="text-center">Type</th>
+                            <th class="text-center">Links</th>
+                            <th class="text-center">Edit Details</th>
+                            <th class="text-center">Remove Book</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <!-- Title -->
+                            <td>
+
+                            </td>
+                            <!-- Author -->
+                            <td>
+
+                            </td>
+                            <!-- Publisher -->
+                            <td>
+
+                            </td>
+                            <!-- Year -->
+                            <td>
+
+                            </td>
+                            <!-- Type -->
+                            <td>
+
+                            </td>
+                            <!-- Links -->
+                            <td>
+
+                            </td>
+                            <!-- Actions -->
+                            <td>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                </template>
+            </v-simple-table>
+        </v-row>
+    </div>
 </template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
